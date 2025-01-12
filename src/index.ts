af9fd9e150c5b90ffecf949e023ea558edbaa084
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-01-13 02:23:36 +0800

    feat(circle): resize circles based on visitor counts and different years

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "leaflet/dist/leaflet.css";
 
 import data from "../data.json";
 
+// component settings
 const map = L.map("map").setView([25.077855115246464, 121.55387115519812], 12);
 
 // 底圖
@@ -12,17 +13,38 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const geojson = L.geoJSON(
+const abs_year = document.getElementById("abs_year") as HTMLSelectElement;
+
+let geojson: L.GeoJSON | undefined = undefined;
+
+function updateGeojson() {
+  if (geojson) {
+    map.removeLayer(geojson);
+  }
+
+  geojson = L.geoJSON(
   // @ts-ignore
   data,
   {
     pointToLayer: function (geoJsonPoint, latlng) {
-      return L.circleMarker(latlng, { radius: 10 });
+        return L.circleMarker(latlng, { radius: Math.log(geoJsonPoint.properties[abs_year.value + "年"] / 100000) / Math.log(7) * 17 });
     },
     onEachFeature: function (feature, layer) {
-      layer.bindPopup(feature.properties.name);
+        layer.bindPopup(feature.properties.名稱 + "\n" + feature.properties[abs_year.value + "年"] + "人");
+        layer.bindTooltip(feature.properties.名稱);
     },
   }
 );
 
 geojson.addTo(map);
+}
+
+// element hooks
+abs_year.addEventListener("change", updateGeojson);
+
+//lifespan hooks
+function onMounted() {
+  updateGeojson();
+}
+
+onMounted();
