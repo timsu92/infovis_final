7eabcbef52c605e54dbaf7788107f57170c84515
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-01-13 00:45:43 +0800

    feat(icon): use circles to mark locations

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 import data from "../data.json";
-import markerIcon from "./markerIcon";
-
-L.Marker.prototype.options.icon = markerIcon;
 
 const map = L.map("map").setView([25.077855115246464, 121.55387115519812], 12);
 
@@ -19,15 +16,8 @@ const geojson = L.geoJSON(
   // @ts-ignore
   data,
   {
-    style: function (feature) {
-      return {
-        // fillColor: feature.properties.color,
-        weight: 2,
-        opacity: 1,
-        color: "white",
-        dashArray: "3",
-        fillOpacity: 0.7,
-      };
+    pointToLayer: function (geoJsonPoint, latlng) {
+      return L.circleMarker(latlng, { radius: 10 });
     },
     onEachFeature: function (feature, layer) {
       layer.bindPopup(feature.properties.name);
