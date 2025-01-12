8014709ce4bd40da5e54055cdaf854f503838437
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-01-12 22:55:59 +0800

    build(vite): preview using vite

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+import data from "../data.json";
+
 const map = L.map("map").setView([25.077855115246464, 121.55387115519812], 12);
 
+// 底圖
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+const geojson = L.geoJSON(data, {
+  style: function (feature) {
+    return {
+      // fillColor: feature.properties.color,
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
+  },
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.name);
+  }
+});
+
+geojson.addTo(map);
